088f8d93bb1a497281a890fff3f084ac9a29a5d4
Andrew Joslin <andrewtjoslin@gmail.com>
2016-04-21 09:25:04 -0600

    Add width and height properties

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ module.exports = function scrollPosition (element) {
   if (!element || element === window) {
     return {
       x: window.scrollX,
-      y: window.scrollY
+      y: window.scrollY,
+      height: window.innerHeight,
+      width: window.innerWidth
     }
   }
 
   return {
     x: element.scrollLeft,
-    y: element.scrollTop
+    y: element.scrollTop,
+    width: element.scrollWidth,
+    height: element.scrollHeight
   }
 }
