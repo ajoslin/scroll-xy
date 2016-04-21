30ccdbfe83122b4185b536665df2e911acf9ba34
Andrew Joslin <andrewtjoslin@gmail.com>
2016-04-21 10:47:55 -0600

    Add maximumX and maximumY to result

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 'use strict'
 
 var window = require('global/window')
+var document = require('global/document')
 
 module.exports = function scrollPosition (element) {
   if (!element || element === window) {
     return {
       x: window.scrollX,
       y: window.scrollY,
-      height: window.innerHeight,
-      width: window.innerWidth
+      maximumX: document.documentElement.scrollWidth - window.innerWidth,
+      maximumY: document.documentElement.scrollHeight - window.innerHeight
     }
   }
 
   return {
     x: element.scrollLeft,
     y: element.scrollTop,
-    width: element.scrollWidth,
-    height: element.scrollHeight
+    maximumX: element.scrollWidth - element.offsetWidth,
+    maximumY: element.scrollHeight - element.offsetHeight
   }
 }
